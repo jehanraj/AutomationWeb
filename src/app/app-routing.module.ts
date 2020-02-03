00143f49c2b7214d10b29e32d4a98968007b819f
JeyaPrakash <jeyaprakash.s@thirdware.com>
2020-02-03 19:56:01 +0530

    Application Details change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { TestSuiteMaintenanceComponent } from './test-suite-maintenance/test-suite-maintenance.component';
+import { ApplicationDetailsComponent } from './application-details/application-details.component';
 
 const routes: Routes = [{ path: 'login', component: LoginComponent },
 { path: '', component: HomeComponent },
-{ path: 'testsuitemaintenance', component: TestSuiteMaintenanceComponent }];
+{ path: 'testsuitemaintenance', component: TestSuiteMaintenanceComponent },
+{ path: 'applicationdetails', component: ApplicationDetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
