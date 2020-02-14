aca9452e00dc8437e8bb90f9bfa0a0d568db3490
Arockiajeganraj A <Arockiajeganraj.A@thirdware.com>
2020-02-14 17:43:21 +0530

    routing for new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestSuiteMaintenanceComponent } from './test-suite-maintenance/test-suite-maintenance.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { TestedReportsComponent } from './tested-reports/tested-reports.component';
-import { TestSuiteMaintenanceComponent } from './test-suite-maintenance/test-suite-maintenance.component';
 import { ApplicationDetailsComponent } from './application-details/application-details.component';
+import { TestSuiteCreationComponent } from './test-suite-creation/test-suite-creation.component';
+import { TestedReportsComponent } from './tested-reports/tested-reports.component';
 import { TestcaseMaintenanceComponent } from './testcase-maintenance/testcase-maintenance.component';
 import { AuthenticationGuard } from './shared/authentication.guard';
 
@@ -14,7 +15,6 @@ const routes: Routes = [{ path: 'login', component: LoginComponent },
 { path: 'testcasemaintenance', component: TestcaseMaintenanceComponent, canActivate: [AuthenticationGuard] },
 { path: 'applicationdetails', component: ApplicationDetailsComponent, canActivate: [AuthenticationGuard] },
 { path: 'loadTestReportDetails', component: TestedReportsComponent, canActivate: [AuthenticationGuard] }];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
