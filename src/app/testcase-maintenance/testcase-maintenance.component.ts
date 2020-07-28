b0a5481c346133fb5f10501797b15f15c7328a3d
JeyaPrakash <jeyaprakash.s@thirdware.com>
2020-07-28 15:32:09 +0530

    Test case excel download template

--- src/app/testcase-maintenance/testcase-maintenance.component.ts
+++ src/app/testcase-maintenance/testcase-maintenance.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { Lookup } from '../shared/app.model';
 import { ToastrService } from 'ngx-toastr';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-testcase-maintenance',
@@ -23,13 +25,14 @@ export class TestcaseMaintenanceComponent implements OnInit {
   uploadedFileName: string;
   url;
 
-  constructor(private app: AppService, private toastr: ToastrService, private sanitizer: DomSanitizer) { }
+  constructor(private app: AppService, private toastr: ToastrService, private sanitizer: DomSanitizer, private http: HttpClient) { }
 
   ngOnInit() {
     this.applicationList = this.app.getApplications();
     this.app.getScreens().subscribe(data => {
       this.screenMap = data;
     });
+    this.downloadTemplate();
   }
 
   updateScreensList() {
@@ -63,6 +66,14 @@ export class TestcaseMaintenanceComponent implements OnInit {
     }, (error) => {
       console.log('File not found');
       this.fileName = '';
+    });
+  }
+
+  downloadTemplate() {
+    this.http.get(environment.baseurl + 'downloadTestCaseTemplate/', { responseType: 'blob' }).subscribe(data => {
+      const file = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;' });
+      this.url = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(file));
+      this.fileName = 'TestCase_Project_Screen.xlsx';
     });
   }
 }
